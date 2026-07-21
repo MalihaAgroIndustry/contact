c2b96c1eae24fe3bbf8cb588dc9dec3f6cffa244
Maliha Agro Industry <malihaindustry@gmail.com>
2026-07-21 20:19:25 +0600

    script.js

--- script.js
+++ script.js
@@ -150,3 +150,34 @@ if (searchInput) {
     });
 
 }
+
+// Animated Counter
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const update = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const speed = target / 80;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + speed);
+
+            setTimeout(update,20);
+
+        }else{
+
+            counter.innerText = target + "+";
+
+        }
+
+    };
+
+    update();
+
+});
